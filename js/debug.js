e1dcf8009fa6e377a5f013653d5de2f5bd6eb4f1
Minin <70429604+MininxD@users.noreply.github.com>
2023-09-30 21:49:08 +0700

    Add files via upload

--- js/debug.js
+++ js/debug.js
@@ -1,1 +1,31 @@
+import {
+  inputSeed,
+  model,
+  scheduler,
+  generateBtn,
+  fileType,
+} from "../exportVar.js";
 
+import { inference, guide } from "../components/eventListener.js";
+
+export function debug() {
+  let modelText = model.options[model.selectedIndex].text;
+  let schedulerText = scheduler.options[scheduler.selectedIndex].text;
+
+  document.getElementById("formdata").innerHTML =
+    '{"prompt":' +
+    '"' +
+    input.value +
+    '", "seed":"' +
+    parseInt(inputSeed.value) +
+    '", "inference_steps":"' +
+    parseInt(inference.value) +
+    '", "guidelines":"' +
+    guide.value +
+    '", "model_name:"' +
+    modelText +
+    '", "scheduler:"' +
+    schedulerText +
+    '"}<br>';
+}
+debug();
